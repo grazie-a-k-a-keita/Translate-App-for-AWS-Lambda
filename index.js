0a5a9a7211d3b159e466b64f10c0ef5538553b9c
Grazie <106722155+grazie-a-k-a-keita@users.noreply.github.com>
2023-06-10 15:30:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
+var AWS = require('aws-sdk');
+var translate = new AWS.Translate();
+var dynamodb = new AWS.DynamoDB();
 
+exports.handler = async (event) => {
+    const inputText = event.queryStringParameters.input_text
+    const outputText = await getTranslatedText(inputText);
+    
+    await putItem(inputText, outputText)
+    
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET"
+        },
+        body: JSON.stringify({
+            inputText: inputText,
+            outputText: outputText
+        }),
+        isBase64Encoded: false
+    };
+    return response;
+};
+
+// 翻訳する関数
+function getTranslatedText(inputText) {
+    return new Promise(((resolve, reject) => {
+        var params = {
+            SourceLanguageCode: 'ja',
+            TargetLanguageCode: 'en',
+            Text: inputText,
+        };
+        
+        translate.translateText(params, function(err,data){
+            if (err) {
+                console.log(err);
+                reject();
+            } else {
+                console.log(JSON.stringify(data));
+                resolve(data.TranslatedText);
+            }
+        });
+    }));
+};
+
+// DynamoDBに更新する関数
+function putItem(inputText, outputText) {
+    var timestamp = new Date().getTime();
+    var params = {
+        Item: {
+            "timestamp": {
+                S: String(timestamp)
+            }, 
+            "input_text": {
+                S: inputText
+            }, 
+            "output_text": {
+                S: outputText
+            }
+        }, 
+        ReturnConsumedCapacity: "TOTAL", 
+        TableName: "handson-table"
+    };
+
+    dynamodb.putItem(params, function(err, data) {
+        if (err) console.log(err);
+        else     console.log(data);
+    });
+}
